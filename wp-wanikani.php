2e8bd865fc64790eca650ad3c2f1caa89f989801
Ryan Plas <rplas@crowdfavorite.com>
2017-04-07 20:44:17 -0400

    Add plugin header

---
+++ wp-wanikani.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP WaniKani
+Plugin URI:  https://wordplas.com
+Description: Widgets for displaying WaniKani stats
+Version:     0.1.0
+Author:      Ryan Plas
+Author URI:  https://wordplas.com
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wp-wanikani
+*/
